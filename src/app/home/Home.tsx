7c0cbe35e5acd8478d4ffb531a1b1d5c3f3f4f6a
Edison_Petter <edison_petter@dell.com>
2021-06-14 23:37:14 -0300

    changing logo

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -4,7 +4,7 @@ import './Home.css';
 import JoinContainer from './join-container/JoinContainer';
 import UserName from './UserName/UserName';
 
-import appLogo from '../assets/speech-bubble-svgrepo-com.svg'
+import appLogo from '../assets/bubble-logo5.png'
 
 interface HomeProps {
     user: User
@@ -28,7 +28,7 @@ function Home({ user, showNewChatOptions, joinConversationByLink, editUsername }
 
       <div className="home-container">
         <div style={{margin:25}}>
-          <img style={{height:100}} alt="logo" src={appLogo}/>
+          <img style={{height:90}} alt="logo" src={appLogo}/>
         </div>
         <UserName user={user} editUsername={editUsername} />
         <div>
